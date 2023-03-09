06112a7231a027af6b9deb2abe3412003831cde1
Robert Cinciuc <robertcinciuc@gmail.com>
2023-03-09 10:49:30 +0100

    Added region tmp (no metadata file generated)

---
+++ Assets/Scripts/Map/RegionTmp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionTmp : MonoBehaviour {
+    
+    private int radiusOut;
+    private int radiusIn = Mathf.sqrt(3)/2 * radiusOut;
+    private int centerPos;
+    // private Object drawing;
+    // Get a random drawing from the collection of drawings that can fit in the space of this region
+    
+    void Start(){
+    }
+
+    void Update(){
+        
+    }
+}
