99369517d05a13538d69ddc9a05c1062e4a6c2b5
Robert Cinciuc <robertcinciuc@gmail.com>
2023-03-21 10:49:26 +0100

    Added classes for region features (8 features in total)

---
+++ Assets/Scripts/Map/RegionFeatures/CliffTopTmp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CliffTop : Feature {
+    
+    void Start() {
+    }
+
+    void Update() {
+    }
+
+    public override string getName() {
+        return "CliffTop";
+    }
+}
---
+++ Assets/Scripts/Map/RegionFeatures/LakeTmp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lake : Feature {
+    
+    void Start() {
+    }
+
+    void Update() {
+    }
+
+    public override string getName() {
+        return "Lake";
+    }
+}
