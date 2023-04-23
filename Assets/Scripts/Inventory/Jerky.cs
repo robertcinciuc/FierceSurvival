f0f4911ebd99b65578179d510fee68e08f94357b
Robert Cinciuc <robertcinciuc@gmail.com>
2023-04-23 18:41:16 +0200

    Removed NourishmentItem class

--- Assets/Scripts/Inventory/Jerky.cs
+++ Assets/Scripts/Inventory/Jerky.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jerky : NourishmentItem {
+public class Jerky : Object {
 
     public static int nourishmentValue = 20;
 
